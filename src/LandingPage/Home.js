730a5444969fa5d87f7fc488a4d2611eac4016ad
nihil27011996 <pellissery.n@northeastern.edu>
2025-04-08 13:49:45 -0700

    "updated scroll into view"

--- src/LandingPage/Home.js
+++ src/LandingPage/Home.js
@@ -1,31 +1,18 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./home.module.css";
 import PersonalizedComponent from "./Components/PersonalizedComponent";
 import Banner from "./Components/Banner";
 import Suggestions from "./Components/Suggestions";
 import Features from "./Components/Features";
 import Events from "./Components/Events";
-import { useRef } from "react";
 const Home = () => {
   const scrollIntoSection = (element) => {
-    const headerOffset = 70; // Static header height
-    console.log(element);
-    // Ensure we use the correct element to retrieve data-section-id
-    const sectionId = element.dataset.sectionId;
-    if (sectionId) {
-      const sectionToScrollTo = document.getElementById(sectionId);
-
-      if (sectionToScrollTo) {
-        const elementPosition = sectionToScrollTo.offsetTop;
-        const offsetPosition = elementPosition - headerOffset;
-        window.scrollTo({
-          top: offsetPosition,
-          behavior: "smooth",
-        });
-      }
+    const sectionToScrollTo = document.getElementById(element);
+    console.log(sectionToScrollTo);
+    if (sectionToScrollTo) {
+      sectionToScrollTo.scrollIntoView({ behavior: "smooth" });
     }
   };
-
   return (
     <div className={styles.homeContainer}>
       <header>
@@ -37,18 +24,27 @@ const Home = () => {
             <li
               onClick={(e) => {
                 e.preventDefault();
-                // Ensure you're using the closest li if there are nested elements
-                const targetLi = e.target.closest("li[data-section-id]");
-                if (targetLi) {
-                  scrollIntoSection(targetLi);
-                }
+                scrollIntoSection("suggestions");
               }}
-              data-section-id="Suggestions"
             >
               Suggestions
             </li>
-            <li>Features</li>
-            <li>Events</li>
+            <li
+              onClick={(e) => {
+                e.preventDefault();
+                scrollIntoSection("Features");
+              }}
+            >
+              Features
+            </li>
+            <li
+              onClick={(e) => {
+                e.preventDefault();
+                scrollIntoSection("Events");
+              }}
+            >
+              Events
+            </li>
           </div>
 
           <div className={styles.pushright}>
@@ -57,15 +53,18 @@ const Home = () => {
                 Login
               </Link>
             </li>
-
-            <li>Sign up</li>
+            <li>
+              <Link to="/createProfile" className={styles.link}>
+                Sign up
+              </Link>
+            </li>
           </div>
         </ul>
       </header>
       <main className={styles.mainContainer}>
         <Banner />
         <PersonalizedComponent />
-        <Suggestions id="Suggestions" />
+        <Suggestions />
         <Features />
         <Events />
       </main>
