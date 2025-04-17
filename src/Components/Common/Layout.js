9de8d5f629f1386d0706c8c3eb4f47eab9f7adf4
nihil27011996 <pellissery.n@northeastern.edu>
2025-04-16 20:08:00 -0700

    "add protected routes"

--- src/Components/Common/Layout.js
+++ src/Components/Common/Layout.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import DriverNavBar from "../../Navbar/driver/navBarComponent-driver";
 import RiderNavbar from "../../Navbar/rider/navBarComponent-rider";
 import styles from "./Layout.module.css";
+import { Outlet } from "react-router-dom";
+
 const MainContainer = styled.div`
   height: 100%;
   width: 100%;
@@ -18,7 +20,7 @@ const Layout = ({ userType, children, userData }) => {
       ) : (
         <></>
       )}
-      {children}
+      <Outlet />
     </>
   );
 };
